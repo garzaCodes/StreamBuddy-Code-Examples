a9268de51285d6fcee6a88a15bf3188b1403ba0a
Marc Garza <marcgarza@garzasaysmbp.lan>
2022-05-20 00:09:02 -0400

    Initial

---
+++ closure/closure.ex.js
@@ -0,0 +1,81 @@
+function openBank(name) {
+  let nextAccountNumber = 721710;
+  const BankName = name;
+  const accounts = {};
+
+  return {
+    getAccount,
+    openAccount,
+  };
+
+  function getAccount(accountNumber) {
+    const account = accounts[accountNumber];
+    return account ? account : "No account exists with that number";
+  }
+
+  function createNewAccount(accountHolderName) {
+    const accountNo = (nextAccountNumber += 1);
+    const accountInfo = {
+      accountHolderName: accountHolderName,
+      transactionHistory: [],
+      bank: name,
+      balance: 0,
+      accountNo,
+    };
+
+    console.log("Account Created: ", accountInfo);
+
+    return (accounts[accountNo] = accountInfo);
+  }
+
+  function openAccount(name, initialDeposit = 0) {
+    const accountRef = createNewAccount(name);
+
+    if (initialDeposit) {
+      deposit(initialDeposit);
+    }
+
+    return {
+      deposit,
+      withdraw,
+      checkBalance,
+      printStatement,
+    };
+
+    function deposit(depositAmount) {
+      accountRef.balance += depositAmount;
+      return !!recordTransaction("Deposit", depositAmount);
+    }
+
+    function withdraw(withdrawAmount) {
+      accountRef.balance -= withdrawAmount;
+      return !!recordTransaction("Withdraw", withdrawAmount);
+    }
+
+    function checkBalance() {
+      console.log(accountRef);
+    }
+
+    function recordTransaction(type, amount) {
+      const transaction = {
+        accountHolderName: accountRef.accountHolderName,
+        newBalance: accountRef.balance,
+        bankName: BankName,
+        date: new Date(),
+        amount,
+        type,
+      };
+
+      accountRef.transactionHistory = [
+        ...accountRef.transactionHistory,
+        transaction,
+      ];
+
+      return true;
+    }
+
+    function printStatement() {
+      console.log("Transaction History: ", accountRef.transactionHistory);
+    }
+  }
+}
---
+++ recursion/recursion.ex.js
@@ -0,0 +1,20 @@
+const scope = { message: "Not finished yet", finished: "Finished..." };
+const maxRounds = 10;
+let rounds = 0;
+
+function processRecords() {
+  const message = "Not finished yet";
+  const finished = "Finished...";
+
+  if (rounds === maxRounds) {
+    console.log(finished, "All Execution Contexts Closed");
+    return;
+  }
+  rounds++;
+
+  console.log(`Execution Context ${rounds} Created: `, scope);
+
+  return processRecords();
+}
+
+processRecords();
